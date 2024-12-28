632378affcb7f61e56f8cee6fca43d74120d3b7a
DetermiedMech1 <DetermiedMech1@gmail.com>
2024-12-28 03:02:21 -0600

    made box drawing more reliable for longer words/terms

--- main.nim
+++ main.nim
@@ -16,18 +16,13 @@ shuffle(words)
 
 ui.drawLoop(objects, 10):
   var i = -1
+  let longest = words.sortedByIt(it.len)[^1]
+
   objects = collect:
     for word in words:
       inc i
       Box(content: word,
-          x: (i mod 4)*13, y: (i div 4)*5,
-          w: words.sortedByIt(it.len)[^1].len, h: 2).render(choices.contains word)
-  
-  choices.add stdin.readLine()
-  if choices.len == 4: 
-    words = (game.checkOptions(answers,choices))[0]
-    let ok = (game.checkOptions(answers,choices))[1]
-    echo ok
-    choices = @[]
+          x: (i mod 4)*(longest.len+4), y: (i div 4)*5,
+          w: longest.len+2, h: 2).render(choices.contains word)
   
 
